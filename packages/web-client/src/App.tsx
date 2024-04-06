9707762397344ce9bf808a9ac050275d94be45e6
StephenFeddes <feddessteve@gmail.com>
2024-04-05 21:46:54 -0500

    Testing client CI/CD

--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -3,7 +3,6 @@ import { AuthProvider } from './modules/shared';
 import { AppRoutes } from './routes/AppRoutes';
 import { AppRoutesMobile } from './routes/AppRoutesMobile';
 
-// Entry point
 export const App = () => {
     return (
         <AuthProvider>
