b35553906070b33039a8bd6f343197bcd7a72114
StephenFeddes <feddessteve@gmail.com>
2024-04-11 17:40:55 -0500

    Testing CI/CD

--- packages/web-client/src/modules/signup/mobile-screen/SignupPageMobile.tsx
+++ packages/web-client/src/modules/signup/mobile-screen/SignupPageMobile.tsx
@@ -1,5 +1,6 @@
 import { SignupFormMobile } from "./features/SignupFormMobile";
 
+// Mobile version for signup page.
 export const SignupPageMobile = () => {
     return (
         <SignupFormMobile />
