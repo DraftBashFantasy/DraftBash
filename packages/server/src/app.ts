82bae9a3bb017d1a724bfbfbd714be4167bbe69a
StephenFeddes <feddessteve@gmail.com>
2024-05-04 19:35:45 -0500

    Testing CI/CD

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import { expressApp } from './rest-api/expressApp';
 
+// Entry point for the server
 export const httpServer = http.createServer(expressApp);
 
 const PORT = process.env.PORT || 3000;
